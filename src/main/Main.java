f60d8bc2e5202d97e7b19c850885586c44464bcc
yhdev7935 <qodydgk8624@kookmin.ac.kr>
2020-06-09 09:48:47 +0900

    implement: find Prime Implicants (Complete)

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
@@ -14,6 +15,9 @@ public class Main {
 	public static int mintermSize;
 	public static int dcsize;
 	public static Minterm[] minterm;
+	public static ArrayList<Integer> dontCareNumbers = new ArrayList<Integer>();
+	public static Minterm[] notCombinedMinterm;
+	public static int[] Combined;
 	
 	public static void main(String[] args)
 	{
@@ -46,44 +50,133 @@ public class Main {
 		dcsize = scan.nextInt();
 		
 		minterm = new Minterm[mintermSize + dcsize]; 
+		Combined = new int[mintermSize + dcsize];
 		
 		for(int i = 0; i < mintermSize; i++)
 		{
 			System.out.printf("Minterm [%d] > ", i + 1);
 			int t = scan.nextInt();
-			minterm[i] = new Minterm(t);
+			minterm[i] = new Minterm(t, binarySize);
+			dontCareNumbers.add(i);
 		}
 		
 		for(int i = 0; i < dcsize; i++)
 		{
 			System.out.printf("Don't Care [%d] > ", i + 1);
 			int t = scan.nextInt();
-			minterm[mintermSize + i] = new Minterm(t);
+			minterm[mintermSize + i] = new Minterm(t, binarySize);
 			minterm[mintermSize + i].setDontCare(true);
 		}
 		
 		// sorting
-		ArrayList<Minterm> mL = new ArrayList<Minterm>(Arrays.asList(minterm));
-		Collections.sort(mL);
-		minterm = mL.toArray(new Minterm[mL.size()]);
+		Collections.sort(dontCareNumbers);
+		minterm = Minterm.sort(minterm);
 		
 		/* Step 1 ////////////////////////////////////////
 		 * Find Prime Implicants
 		 */
-		findPI();
-	}
-	
-	public static void findPI()
-	{
 		System.out.println("######################################");
 		System.out.println("Step 1. Find Prime Implicants");
 		System.out.println("######################################\n");
-		System.out.println("# of 1s | Minterm | Binary");
+		System.out.println("\n##############Initialize##############\n");
+		findImplicants();
+		System.out.println("\n###############Combine################\n");
+		boolean flag = true;
+		while(flag)
+			flag = combineImplicants(flag);
+	}
+	
+	public static void findImplicants()
+	{
+		System.out.println("######################################");
+		System.out.println("# of 1s | Minterm | Binary | Combined");
 		for(int i = 0; i < minterm.length; i++)
-			System.out.printf("%d | %s | %s\n", 
+			System.out.printf("%d | %s | %s | %s\n", 
 					minterm[i].getNumberOfOnes(),
-					minterm[i].getTermsNumber(),
-					minterm[i].toString());
+					minterm[i].getTermsNumberString(),
+					minterm[i].toString(),
+					Combined[i] == 1 ? "V" : "");
+		System.out.println("######################################");
+	}
+	
+	public static boolean combineImplicants(boolean flag)
+	{
+		if(minterm.length == 0) return false;
+		
+		boolean escape = false;
+		Combined = new int[minterm.length];
+		int max_ones = minterm[minterm.length - 1].getNumberOfOnes();
+		
+		ArrayList<Minterm> CombinedMinterm = new ArrayList<Minterm>(); HashSet<String> duplicate = new HashSet<String>();
+		for(int i = 0; i < max_ones; i++)
+		{
+			Minterm[] minterms_a = Minterm.getMinterms(minterm, i);
+			Minterm[] minterms_b = Minterm.getMinterms(minterm, i + 1);
+			for(int ai = 0; ai < minterms_a.length; ai++)
+			{
+				for(int bi = 0; bi < minterms_b.length; bi++)
+				{
+					Minterm minterm_ai = minterms_a[ai];
+					Minterm minterm_bi = minterms_b[bi];
+					//System.out.printf("합치기중... %s[idx: %d] + %s[idx: %d] , HD: %d\n", minterm_ai.toString(),
+					//		Minterm.getIndexOfMintermGroups(minterm, i, ai), minterm_bi.toString(),
+					//		Minterm.getIndexOfMintermGroups(minterm, i + 1, bi),
+					//		Minterm.getHammingDistance(minterm_ai, minterm_bi));
+					if(Minterm.getHammingDistance(minterm_ai, minterm_bi) == 1)
+					{
+						escape = true;
+						Minterm newMinterm = new Minterm(minterm_ai, minterm_bi);
+						Combined[Minterm.getIndexOfMintermGroups(minterm, i, ai)] = 1;
+						Combined[Minterm.getIndexOfMintermGroups(minterm, i + 1, bi)] = 1;
+						
+						if(!duplicate.contains(newMinterm.toString()))
+						{
+							duplicate.add(newMinterm.toString());
+							CombinedMinterm.add(newMinterm);
+						}
+					}
+				}
+			}
+		}
+		
+		duplicate = new HashSet<String>();
+		for(int i = 0; i < minterm.length; i++)
+			if(Combined[i] == 0 && !duplicate.contains(minterm[i].toString()))
+			{
+				duplicate.add(minterm[i].toString());
+				
+				ArrayList<Minterm> mL;
+				if(notCombinedMinterm != null && notCombinedMinterm.length != 0) mL = new ArrayList<Minterm>(Arrays.asList(notCombinedMinterm));
+				else mL = new ArrayList<Minterm>();
+				mL.add(minterm[i]);
+				notCombinedMinterm = mL.toArray(new Minterm[mL.size()]);
+			}
+		
+		Minterm.sort(notCombinedMinterm);
+		
+		if(escape == false) return false;
+		
+		System.out.println("\n################Before################\n");
+		findImplicants(); // before
+		
+		minterm = CombinedMinterm.toArray(new Minterm[CombinedMinterm.size()]);
+		Combined = new int[minterm.length];
+		
+		System.out.println("\n################After#################\n");
+		findImplicants(); // after
+		System.out.print("\n\n\n");
+		
+		return escape;
+		
+	}
+	
+	public static int countCombinedImplicants()
+	{
+		int ret = 0;
+		for(int i = 0; i < Combined.length; i++)
+			if(Combined[i] == 1)
+				ret += 1;
+		return ret;
 	}
 
 
--- src/main/Minterm.java
+++ src/main/Minterm.java
@@ -1,6 +1,11 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Minterm implements Comparable<Minterm>{
 	
@@ -8,16 +13,44 @@ public class Minterm implements Comparable<Minterm>{
 	private boolean dontcare = false;
 	private ArrayList<Integer> termNumber = new ArrayList<Integer>();
 	private String term = "";
+	private int numberOfOnes = -1;
 
-	public Minterm(int num)
+	public Minterm(int num, int binarySize)
 	{
 		term = Integer.toBinaryString(num);
 		termNumber.add(num);
+		
+		if(term.length() != binarySize)
+		{
+			String s = "";
+			for(int i = 0; i < binarySize - term.length(); i++)
+				s += "0";
+			term = s + term;
+		}
 	}
 	
 	public Minterm(Minterm m0, Minterm m1)
 	{
+		String m0_str = m0.toString();
+		String m1_str = m1.toString();
+		for(int i = 0; i < m0_str.length(); i++)
+		{
+			char m0i = m0_str.charAt(i);
+			char m1i = m1_str.charAt(i);
+			if(m0i == m1i) term += m0i;
+			else term += '-';
+		}
 		
+		HashSet<Integer> hs = new HashSet<Integer>();
+		for(int i = 0; i < m0.getTermsNumber().size(); i++)
+			hs.add(m0.getTermsNumber().get(i));
+		for(int i = 0; i < m1.getTermsNumber().size(); i++)
+			hs.add(m1.getTermsNumber().get(i));
+		
+		Iterator<Integer> hs_iter = hs.iterator();
+		while(hs_iter.hasNext()) termNumber.add(hs_iter.next());
+		
+		Collections.sort(termNumber);
 	}
 	
 	public boolean isDontCare()
@@ -37,14 +70,24 @@ public class Minterm implements Comparable<Minterm>{
 	
 	public int getNumberOfOnes()
 	{
-		int ret = 0;
-		for(int i = 0; i < term.length(); i++)
-			if(term.charAt(i) == '1')
-				ret++;
-		return ret;
+		if(this.numberOfOnes == -1) // 처음만 갱신
+		{
+			int ret = 0;
+			for(int i = 0; i < term.length(); i++)
+				if(term.charAt(i) == '1')
+					ret++;
+			this.numberOfOnes = ret;
+			return ret;
+		}
+		else return this.numberOfOnes;
 	}
 
-	public String getTermsNumber()
+	public ArrayList<Integer> getTermsNumber()
+	{
+		return this.termNumber;
+	}
+	
+	public String getTermsNumberString()
 	{
 		String ret = "";
 		for(int i = 0 ; i < termNumber.size(); i++)
@@ -55,13 +98,77 @@ public class Minterm implements Comparable<Minterm>{
 		}
 		return ret;
 	}
+	
+	public int sumOfTermsNumber()
+	{
+		int ret = 0;
+		for(int i = 0; i < termNumber.size(); i++)
+			ret += termNumber.get(i);
+		return ret;
+	}
+	
 	@Override
 	public int compareTo(Minterm m) {
 		int myOne = this.getNumberOfOnes();
 		int otherOne = m.getNumberOfOnes();
 		if(myOne < otherOne) return -1;
-		else if(myOne == otherOne) return 0;
+		else if(myOne == otherOne)
+		{
+			int myTermSum = this.sumOfTermsNumber();
+			int otherTermSum = m.sumOfTermsNumber();
+			if(myTermSum < otherTermSum) return -1;
+			else if(myTermSum == otherTermSum) return 0; // 사실 일어나면 안되는 경우다.
+			else return 1;
+		}
 		else return 1;
 	
+	}
+	
+	public static int getHammingDistance(Minterm m0, Minterm m1)
+	{
+		int ret = 0;
+		
+		String m0_str = m0.toString();
+		String m1_str = m1.toString();
+		for(int i = 0; i < m0_str.length(); i++)
+		{
+			char m0i = m0_str.charAt(i);
+			char m1i = m1_str.charAt(i);
+			if(m0i == '-' && m1i == '-') ret += 0;
+			else if(m0i != m1i) ret += 1;
+		}
+		
+		return ret;
+	}
+	
+	public static Minterm[] getMinterms(Minterm[] minterms, int ones)
+	{
+		ArrayList<Minterm> ret = new ArrayList<Minterm>();
+		for(int i = 0; i < minterms.length; i++)
+			if(minterms[i].getNumberOfOnes() == ones)
+				ret.add(minterms[i]);
+		return ret.toArray(new Minterm[ret.size()]);
+	}
+	
+	public static Minterm[] sort(Minterm[] minterms)
+	{
+		if(minterms == null || minterms.length == 0) return minterms;
+		ArrayList<Minterm> mL = new ArrayList<Minterm>(Arrays.asList(minterms));
+		Collections.sort(mL);
+		return mL.toArray(new Minterm[mL.size()]);
+	}
+	
+	public static int getIndexOfMintermGroups(Minterm[] minterms, int ones, int idx)
+	{
+		int ret = 0; int i;
+		for(i = 0; i < minterms.length; i++)
+		{
+			if(minterms[i].getNumberOfOnes() == ones)
+			{
+				if(ret == idx) return i;
+				ret++;
+			}
+		}
+		return -1;
 	}
 }
